63935e298cf6e7a3b935f883691e94a133ffb8af
rgt219 <rgt219@outlook.com>
2026-03-04 09:29:09 -0500

    adding more components

---
+++ src/components/Home.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from "react";
+import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import DecksGrid from "./DecksGrid";
+import DeckList from "./DeckList";
+import DeckDetails from "./DeckDetails";
+import Login from "./Login";
+import DeckBuilder from "./DeckBuilder";
+import Footer from "./Footer";
+//import DecksGrid from "./components/DecksGrid";
+
+export default function Home() {
+    const [decks, setDecks] = useState([]);
+    const [decklist, setDeckList] = useState([]);
+
+    useEffect(() => {
+        fetch("decks.json")
+        .then(response => response.json())
+        .then(data => setDecks(data));
+    }, []);
+
+    const toggleDeckList = (deckId) => {
+        setDeckList(prev => 
+            prev.includes(deckId) 
+            ? prev.filter(id => id !== deckId) 
+            : [...prev, deckId]
+        )
+    }
+    return (
+        <>
+        <Carousel className="bg-dark bg-opacity-75 p-2 rounded" style={{width: "100%", height: "515px"}}>
+            <Carousel.Item>
+              <header class="absolute inset-x-0 top-0 z-50">
+                <img className='ygoss' src='./images/ygoss1.jpg' alt="yugioh" />
+              </header>
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+              
+            <Carousel.Item>
+              <header class="absolute inset-x-0 top-0 z-50">
+                  <img className='ygoss' src='./images/ygoss2.jpg' alt="yugioh" />
+              </header>
+            </Carousel.Item>
+
+            <Carousel.Item>
+              <header class="absolute inset-x-0 top-0 z-50">
+                  <img className='ygoss' src='./images/ygoss3.jpg' alt="yugioh" />
+              </header>
+            </Carousel.Item>
+            
+          </Carousel>
+          <div class="p-5 bg-secondary bg-gradient text-white text-white App">
+        <div className="container-box">
+          <Router>
+            <nav>
+              <ul>
+                <Card as={Link} to="/" style={{ width: '18rem', cursor: 'pointer', height: "200px" }} class="hover-card" className="bg-gray-500">
+                  <Card.Img variant="top"  src="./images/lenatus_art.jpg" style={{objectFit: "cover", height: "100%"}}/>
+                  <Card.ImgOverlay>
+                    <Card.Body className="text-center">
+                      <Card.Header style={{backgroundColor: "bg-primary"}}>
+                        <Card.Title className="bg-dark bg-opacity-75 p-2 rounded text-white">Home</Card.Title>
+                      </Card.Header>
+                    </Card.Body>
+                  </Card.ImgOverlay>
+                </Card>
+                <Card as={Link} to="/decklist" style={{ width: '18rem', cursor: 'pointer', height: "200px" }} class="hover-card">
+                  <Card.Img src="./images/droplet_art.jpg" style={{objectFit: "cover", height: "100%"}}/>
+                  <Card.ImgOverlay>
+                    <Card.Body className="text-center">
+                      <Card.Header style={{backgroundColor: "bg-primary"}}>
+                        <Card.Title className="bg-dark bg-opacity-75 p-2 rounded text-white">Deck List</Card.Title>
+                      </Card.Header>
+                    </Card.Body>
+                  </Card.ImgOverlay>
+                </Card>
+                <Card as={Link} to="/deckbuilder" style={{ width: '18rem', cursor: 'pointer', height: "200px" }} class="hover-card">
+                  <Card.Img src="./images/exodia.png" style={{objectFit: "cover", height: "100%"}}/>
+                  <Card.ImgOverlay>
+                    <Card.Body className="text-center">
+                      <Card.Header style={{backgroundColor: "bg-primary"}}>
+                        <Card.Title className="bg-dark bg-opacity-75 p-2 rounded text-white">Deck Builder</Card.Title>
+                      </Card.Header>
+                    </Card.Body>
+                  </Card.ImgOverlay>
+                </Card>
+                {/* <Card as={Link} to="/test" style={{ width: '18rem', cursor: 'pointer' }}>TEST</Card> */}
+              </ul>
+            </nav>
+
+            <Routes>
+              <Route 
+                path="/" 
+                element={<DecksGrid decks={decks} decklist={decklist} toggleDeckList={toggleDeckList}/>}>
+              </Route>
+
+              <Route 
+                path="/decklist" 
+                element={<DeckList decks={decks} decklist={decklist} toggleDeckList={toggleDeckList}/>}>
+              </Route>
+
+              <Route
+                path={"/decks/:deckId"}
+                element={<DeckDetails></DeckDetails>}
+                >
+              </Route>
+              <Route 
+                path="/deckbuilder" 
+                element={<DeckBuilder></DeckBuilder>}>
+              </Route>
+              <Route 
+                path="/login" 
+                element={<Login />}>
+              </Route>
+            </Routes>
+
+          </Router>
+        </div>
+
+
+        <Footer></Footer>
+      </div>
+
+</>)
+}
